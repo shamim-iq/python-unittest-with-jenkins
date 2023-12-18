2ee6cecb8d2366cd638741f94304ffb16308e1a6
Ubuntu <ubuntu@ip-172-31-20-187.ec2.internal>
2023-12-18 14:41:14 +0000

    test success

--- test_website_loader.py
+++ test_website_loader.py
@@ -8,7 +8,7 @@ logger = logging.getLogger(__name__)
 
 class TestWebsiteLoader(unittest.TestCase):
     def test_load_atg_world_website(self):
-        url = "https://atg.worl"
+        url = "https://atg.world"
         logger.info(f"Attempting to load the {url} website...")
         response = None
         try:
